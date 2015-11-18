0bf6b47402793582ac70b283ee06a58ddb3c3aff
Mohamed-Hassan <mah.q82@gmail.com>
2015-11-18 09:18:51 -0500

    Initial Commit

---
+++ RubricGradingAppTests/RubricGradingAppTests.m
@@ -0,0 +1,39 @@
+//
+//  RubricGradingAppTests.m
+//  RubricGradingAppTests
+//
+//  Created by ghg on 11/18/15.
+//  Copyright © 2015 Mohamed Hassan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RubricGradingAppTests : XCTestCase
+
+@end
+
+@implementation RubricGradingAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
